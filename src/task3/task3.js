637d5ee962518a67bff40dd08644b205aa937a48
Anastasiya But-Husaim <anastasiya_but-husaim@epam.com>
2021-10-01 19:55:21 +0300

    add usage of Babel and ES6

---
+++ src/task3/task3.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import readline from 'readline';
+import csv from 'csvtojson';
+import path from 'path';
+
+const file = path.resolve(__dirname,'./csv/file.csv');
+const resultFile = path.resolve(__dirname, './resultFile.txt');
+
+const readStream = fs.createReadStream(file).pipe(csv({}));
+
+const rl = readline.createInterface({
+    input: readStream,
+    crlfDelay: Infinity,
+})
+const writeStream = fs.createWriteStream(resultFile);
+
+rl.on('line', (line) => {
+    writeStream.write(`${line}\n`)
+})
+    .on('error', (error) => {
+        console.log(error);
+    });
